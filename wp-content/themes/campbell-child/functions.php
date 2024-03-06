d2c6063ee4ee811b6169f50b96b7100a839e0787
Hemant Ajay Nagori <hemant.10732637@ltimindtree.com>
2024-03-06 23:27:44 +0530

    Updated functions

--- wp-content/themes/campbell-child/functions.php
+++ wp-content/themes/campbell-child/functions.php
@@ -143,3 +143,7 @@ function movies_hollywood($atts, $content = null) {
     endif;
 }
 add_shortcode('hollywood_movies', 'movies_hollywood');
+
+// Remove Gutenberg Block Editor
+
+add_filter('use_block_editor_for_post', '__return_false');
